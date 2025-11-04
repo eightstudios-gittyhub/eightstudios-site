2533e5f153790f7a93fe33f081bfdc26ead3f413
Dio of 888 <eight888studios@gmail.com>
2025-11-04 15:38:55 -0500

    Create page.jsx black description

---
+++ app/products/app/products/bluebeanie-black/page.jsx
@@ -0,0 +1,17 @@
+export default function BlueBeanieBlack() {
+  return (
+    <main style={{ textAlign: "center", padding: "20px" }}>
+      <h1>888 Blue Beanie — Black Glitter</h1>
+      <img src="https://i.imgur.com/PmFzNSO.jpeg" style={{ width: "80%", maxWidth: "400px", borderRadius: "10px" }} />
+      <img src="https://i.imgur.com/6Nzvtby.jpeg" style={{ width: "80%", maxWidth: "400px", borderRadius: "10px" }} />
+      <img src="https://i.imgur.com/RGjFH54.jpeg" style={{ width: "80%", maxWidth: "400px", borderRadius: "10px" }} />
+      <img src="https://i.imgur.com/5vpvGKp.jpeg" style={{ width: "80%", maxWidth: "400px", borderRadius: "10px" }} />
+      <p style={{ marginTop: "15px", fontSize: "16px" }}>
+        The 888 Blue Beanie (Black Glitter) is a 1/1 handmade design from Eight Studios.
+        Features a distressed blue patch with triple “888” black glitter vinyl on ribbed knit.
+        •888inc. 
+        •size - fits most
+      </p>
+    </main>
+  );
+}
