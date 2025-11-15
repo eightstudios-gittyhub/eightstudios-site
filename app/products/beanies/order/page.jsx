9c57888bef97dd3a013e65c8b378218120d19f90
Dio of 888 <eight888studios@gmail.com>
2025-11-15 17:18:24 -0500

    Update order/page.jsx 2

--- app/products/beanies/order/page.jsx
+++ app/products/beanies/order/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
