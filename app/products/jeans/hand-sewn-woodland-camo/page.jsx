709e6e9265e953c7b7a0fe54899e9515979fb46e
Dio of 888 <eight888studios@gmail.com>
2025-11-30 11:28:16 -0500

    Create page.jsx camo

---
+++ app/products/jeans/hand-sewn-woodland-camo/page.jsx
@@ -0,0 +1,26 @@
+import ProductPage from "@/components/ProductPageTemplate";
+
+export default function Page() {
+  return (
+    <ProductPage
+      title="Hand-Sewn Woodland Camo — 1/1"
+      size="30W / 30L"
+      price="$200"
+      description={`• made by Eight Studios (888inc.) 
+
+• 10hrs of work — all hand sewn (only 10 mins machine sewing). Handmade 1/1 luxury men's jeans.`}
+      images={[
+        "https://i.imgur.com/B3323uR.jpeg",
+        "https://i.imgur.com/fWEzMYF.jpeg",
+        "https://i.imgur.com/nZEsd2Y.jpeg",
+        "https://i.imgur.com/IWebCWw.jpeg",
+        "https://i.imgur.com/NmMwaUu.jpeg",
+        "https://i.imgur.com/8wi095S.jpeg",
+        "https://i.imgur.com/Yf8RQ7n.jpeg",
+        "https://i.imgur.com/hrG4HKq.jpeg",
+        "https://i.imgur.com/yU86GOT.jpeg",
+        "https://i.imgur.com/O9sgchl.jpeg",
+      ]}
+    />
+  );
+}
