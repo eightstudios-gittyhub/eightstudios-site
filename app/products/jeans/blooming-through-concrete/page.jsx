5363037bb818c370b4807a4bdbac22dcb2da0752
Dio of 888 <eight888studios@gmail.com>
2025-11-30 11:59:42 -0500

    Update page.jsx blooming

--- app/products/jeans/blooming-through-concrete/page.jsx
+++ app/products/jeans/blooming-through-concrete/page.jsx
@@ -4,9 +4,11 @@ export default function BloomingThroughConcrete() {
   return (
     <ProductPageTemplate
       title="Blooming Through Concrete — 1/1 woman’s jeans"
-      size="size — 8 (29–31.5 in waist)"
-      description={`• made by Eight Studios (888inc.)
-• price: $150
+      size="8 (29–31.5 in waist)"
+      price="$155"
+      description={`• price: $155
+• made by Eight Studios (888inc.)
+• made in 2024
 • handmade 1/1 luxury woman’s jeans`}
       images={[
         "https://i.imgur.com/zCDwOmK.jpeg",
