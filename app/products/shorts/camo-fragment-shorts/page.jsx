06a58a376290e18a3ccb8167725f1096fd30cb24
Dio of 888 <eight888studios@gmail.com>
2025-11-30 16:42:34 -0500

    Create page.jsx shorts camo

---
+++ app/products/shorts/camo-fragment-shorts/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import ProductPage from "@/components/ProductPageTemplate";
+
+export default function Page() {
+  return (
+    <ProductPage
+      title="Camo Fragment Shorts — 1/1"
+      size="29W"
+      price="$120"
+      description={`• made by Eight Studios (888inc.)
+• made in 2024
+• was made for commission but the guy didn't want it anymore`}
+      images={[
+        "https://i.imgur.com/Y1zxInG.jpeg",
+        "https://i.imgur.com/f5XfvoN.jpeg",
+        "https://i.imgur.com/XBbNYrD.jpeg",
+        "https://i.imgur.com/849iLws.jpeg",
+        "https://i.imgur.com/sUlgA8c.jpeg",
+        "https://i.imgur.com/HNgNhAH.jpeg",
+        "https://i.imgur.com/kmUnVX3.jpeg",
+        "https://i.imgur.com/d8F3fLo.jpeg",
+      ]}
+    />
+  );
+}
