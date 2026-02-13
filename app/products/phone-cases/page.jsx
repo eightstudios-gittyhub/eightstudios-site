cff3eb848911c4cbf72810cdd8f81ad1e570c118
Dio of 888 <eight888studios@gmail.com>
2026-02-13 12:20:26 -0500

    changed cover image phone case

--- app/products/phone-cases/page.jsx
+++ app/products/phone-cases/page.jsx
@@ -13,7 +13,7 @@ export default function PhoneCasesPage() {
       title: "888Phone Case Midnight Circuit — 1/1",
       price: "$60",
       link: "/products/phone-cases/888phone-case-midnight-circuit-16-plus",
-      img: "https://i.imgur.com/3n1cHwZ.jpeg",
+      img: "https://i.imgur.com/BNHTJQE.jpeg",
       subtitle: "iPhone 16 Plus",
     },
   ];
